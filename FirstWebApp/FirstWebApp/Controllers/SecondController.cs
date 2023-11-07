628d4a20608c7fb029943636f5151bf995b780ef
itiscs <moudroff@gmail.com>
2023-11-07 14:39:02 +0300

    FirstApp

---
+++ FirstWebApp/FirstWebApp/Controllers/SecondController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebApp.Controllers
+{
+    public class SecondController : Controller
+    {
+       // [Route("MyPath")]
+        public IActionResult Index(string id)
+        {
+            if (id != null)
+                ViewData["Name"] = id;
+            return View();
+        }
+    }
+}
