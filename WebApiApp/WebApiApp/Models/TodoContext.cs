3e647b48c94c3704fda71a11ead8342ba3e0a71f
itiscs <moudroff@gmail.com>
2024-04-16 10:46:19 +0300

    Web api

---
+++ WebApiApp/WebApiApp/Models/TodoContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApiApp.Models
+{
+    public class TodoContext:DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+        : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=tododb;Username=postgres;Password=1");
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; } = null!;
+
+    }
+}
